b5076b636fe422e03316d2d14401f622b1a95422
FReptar0 <20213tn137@utez.edu.mx>
2023-03-21 12:35:18 -0600

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAR8kxb9ib33nP8fJysci5-Qd_BAHr4FVY",
+  authDomain: "travel-72d27.firebaseapp.com",
+  projectId: "travel-72d27",
+  storageBucket: "travel-72d27.appspot.com",
+  messagingSenderId: "648892753003",
+  appId: "1:648892753003:web:f2e81fa9e1ad4e83f17d2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
